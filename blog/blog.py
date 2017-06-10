f09a0d66289a5db45b201ca2e855c79f6f4dfb14
Joseph Mtinangi <josephmtinangi@gmail.com>
2017-06-10 17:44:30 +0300

    Create login function

--- blog/blog.py
+++ blog/blog.py
@@ -62,3 +62,18 @@ def store():
     db.commit()
     flash('New post was successfully added')
     return redirect(url_for('index'))
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME']:
+            error = 'Invalid username'
+        elif request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid password'
+        else:
+            session['logged_in'] = True
+            flash('You were logged in')
+            return redirect(url_for('index'))
+    return render_template('auth/login.html', error=error)
