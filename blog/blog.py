f7411e8488836c788ebfeb175a2bba0235b5955e
Joseph Mtinangi <josephmtinangi@gmail.com>
2017-06-10 16:55:07 +0300

    Configure the app

--- blog/blog.py
+++ blog/blog.py
@@ -1,3 +1,16 @@
 import os
 import sqlite3
 from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+app.config.update(dict(
+    DATABASE=os.path.join(app.root_path, 'blog.db'),
+    SECRET_KEY='development key',
+    USERNAME='admin',
+    PASSWORD='default'
+))
+app.config.from_envvar('FLASK_SETTINGS', silent=True)
+
+
